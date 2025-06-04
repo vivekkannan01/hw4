8be35315d0bb9f133f6920f71c7cad3d79c2076d
Vivek Kannan <vivek.kannan@kellogg.northwestern.edu>
2025-06-04 16:26:41 +0000

    fixed the issue with image upload

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,20 +6,21 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = @current_user["id"] #ensures the entry is connected to the logged-in user
-    @entry["image"] = params["image"]  # optional image URL
-
-    if params["uploaded_image"]
-      @entry.uploaded_image.attach(params["uploaded_image"])
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["image"] = params["image"]
+      @entry["user_id"] = @user["id"]
+      @entry.save
+      @entry.uploaded_image.attach(params["uploaded_image"]) if params["uploaded_image"]
+    else
+      flash["notice"] = "Login first."
     end
-    
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    redirect_to "/places/#{params["place_id"]}"
   end
 
 end
