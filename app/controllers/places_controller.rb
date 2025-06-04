1a2a9c53157e99d214b91502b68176b0dd4fe0e4
Vivek Kannan <vivek.kannan@kellogg.northwestern.edu>
2025-06-04 14:50:24 +0000

    fixed the image upload issue and frontend stacking

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,21 +1,28 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.where({ "user_id" => @current_user["user_id"] })
+    @places = Place.where({ "user_id" => @current_user["id"] })
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"], "user_id" => @current_user["user_id"] })
-    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["user_id"] })
+    @place = Place.find_by({ "id" => params["id"], "user_id" => @current_user["id"] })
+
+    # Only show entries for this place that belong to the current user
+    if @place
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+    else
+      redirect_to "/places", notice: "Place not found or access denied."
+    end
   end
 
   def new
+    @place = Place.new
   end
 
   def create
     @place = Place.new
     @place["name"] = params["name"]
-    @place["user_id"] = @current_user["user_id"]
+    @place["user_id"] = @current_user["id"]
     @place.save
     redirect_to "/places"
   end
